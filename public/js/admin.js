2fc81e6275c8bcc3a7b511ab5505fa4e8e660e02
Eher <alexandre@eher.com.br>
2013-09-04 02:07:33 -0300

    Add revgeo call

--- public/js/admin.js
+++ public/js/admin.js
@@ -12,8 +12,14 @@ function updateLocation () {
                     lat: location.coords.latitude, lng: location.coords.longitude
                 }
             }, function (data) {
-                console.log(data);
-                document.getElementById("content").innerText = JSON.stringify(data);
+                $.get("http://maps.googleapis.com/maps/api/geocode/json?latlng="+
+    data.lat+","+data.lng+"&sensor=false", function(data) {
+                    var address = "";
+                    if (data && data.status === "OK") {
+                        address = data.results[0].formatted_address;
+                    }
+                    document.getElementById("content").innerText = address;
+                });
             });
         });
     });
--- public/js/main.js
+++ public/js/main.js
@@ -7,8 +7,14 @@ require.config({
 function getUpdatedLocation () {
     require(["jquery"], function($) {
         $.get("/location", function (data) {
-            console.log(data);
-            document.getElementById("content").innerText = JSON.stringify(data);
+            $.get("http://maps.googleapis.com/maps/api/geocode/json?latlng="+
+data.lat+","+data.lng+"&sensor=false", function(data) {
+                var address = "";
+                if (data && data.status === "OK") {
+                    address = data.results[0].formatted_address;
+                }
+                document.getElementById("content").innerText = address;
+            });
         });
     });
 };
