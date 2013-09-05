9b5f0927fa5806a7cc065b82c41ced8ea5cd92db
Eher <alexandre@eher.com.br>
2013-09-04 21:30:51 -0300

    Disable npm publishing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,11 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        release: {
+            options: {
+                npm: false
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-release');
